e5892b2b22b994ee4f0e37e225c38bee7f4c8c1b
lucas <lucascpinheiro10@gmail.com>
2022-09-21 17:26:18 -0300

    first two chapters

---
+++ chapter-2/src/index.ts
@@ -0,0 +1,11 @@
+// let a = 1 + 2
+// let b = a + 3
+// let c = {
+//     apple: a,
+//     banana: b
+// }
+
+// let d = c.apple * 4
+
+// try to code something that typescript doesn't  figure out the type
+// let x = (1 + [0] * {}) + {}
